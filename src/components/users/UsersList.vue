5dcbdec9f124790789e1b5ddc2988b7eb1b7aff8
Edgar <edgaarr2005@hotmail.com>
2025-04-10 21:04:26 -0400

    Kinda experimenting with router push.

--- src/components/users/UsersList.vue
+++ src/components/users/UsersList.vue
@@ -1,4 +1,5 @@
 <template>
+  <button @click="confirmAction">Confirm</button>
   <ul>
     <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
   </ul>
@@ -12,6 +13,12 @@ export default {
     UserItem,
   },
   inject: ['users'],
+  methods: {
+    confirmAction() {
+      alert('Confirmed!');
+      this.$router.push('/teams');
+    },
+  },
 };
 </script>
 
